0858ae8bb030f09ed75675c0dd33c38a651369a7
JuJiwon <36442946+codingstr@users.noreply.github.com>
2022-01-11 00:20:07 +0900

    주석 추가

--- 타겟 넘버.py
+++ 타겟 넘버.py
@@ -5,15 +5,24 @@ from collections import deque
 def solution(numbers, target):
     answer = 0
     queue = deque([(0, 0)]) # sum, length
+
     while queue :
+
         s, l = queue.popleft()
-        if l > len(numbers):
+        if l > len(numbers) :
             break
-        elif l == len(numbers) and s == target:
+        elif l == len(numbers) and s == target :
             answer += 1
-            print("asdf")
+
+        # 길이를 천천히 늘려가며 sum을 계산.
+        # l==len(numbers)==n일 때 합의 경우의 수는 n^5 (l이 증가할 때마다 합의 경우의 수가 2배씩(+,-) 증가하므로)
         queue.append((s+numbers[l-1], l+1))
         queue.append((s-numbers[l-1], l+1))
+        print('합 = '+str(s))
+        print('길이 = '+str(l))
+        print('numbers[l-1] = '+str(numbers[l-1]))
+        print('queue = '+str(queue))
+        print('---------------------------------------------------------------------------')
 
     return answer
 
