61cf6cebf301088c18963cbbd64c1b3cd9c73bbb
JuJiwon <36442946+codingstr@users.noreply.github.com>
2022-01-11 19:28:47 +0900

    효율성 향상, 주석 추가

--- 단어 변환.py
+++ 단어 변환.py
@@ -4,30 +4,28 @@ from collections import deque
 
 def solution(begin, target, words) :
     answer = 0
-    queue = deque([[begin, 0]])
-
-    def mutableWords(before) :
-        mutableWordsArr = words[:] if begin == before else words[:words.index(before)]+words[words.index(before)+1:]
-        for word in mutableWordsArr[:] :
-            equalNum = 0
-            for i in range(len(word)) :
-                if before[i] == word[i] : equalNum += 1
-            if not equalNum == len(word)-1 : mutableWordsArr.remove(word)
-        return mutableWordsArr
+    queue = deque([[begin, 0]])     # 처리중인 단어, 변환한 횟수
 
     while queue :
-        begin, distance = queue.popleft()
-        mutableWordsArr = mutableWords(begin)
-        print(begin)
+        begin, distance = queue.popleft()       # 처리중인 단어, 변환한 횟수
+        
+        # target으로 변환되었다면 처리
         if begin == target :
             if answer == 0 or answer > distance :
                 answer = distance
+        # 아니라면 다음으로 탐색할 수 있는 단어를 queue에 append
         else :
+            # 처리중인 단어에서 변환할 수 있는 단어들
+            mutableWordsArr = words[:]
+            for word in mutableWordsArr[:] :
+                equalNum = 0
+                for i in range(len(word)) :
+                    if begin[i] == word[i] : equalNum += 1
+                if not equalNum == len(word)-1 : mutableWordsArr.remove(word)
+
             for word in mutableWordsArr :
                 if word in words : words.remove(word)
                 queue.append([word, distance+1])
-        print(list(queue))
-    
 
     return answer
 
